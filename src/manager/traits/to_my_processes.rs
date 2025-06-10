9b2f9653da3db4066705487e036fe5e35442675c
Delnegend <kiennguyen19323@gmail.com>
2025-06-10 22:48:59 +0700

    feat(`manager/traits`): `ToMyProcesses`

---
+++ src/manager/traits/to_my_processes.rs
@@ -0,0 +1,49 @@
+use procfs::{WithCurrentSystemInfo, process::ProcessesIter};
+use tracing::warn;
+
+use crate::manager::{
+    MyProcess,
+    traits::{
+        command_string::CommandString, cpu_percent::CpuPercent,
+        get_procs_using_file::GetUsingFiles, memory_bytes::MemoryBytes, process_name::ProcessName,
+        username::Username,
+    },
+};
+
+pub trait ToMyProcesses {
+    fn to_my_processes(self) -> Vec<MyProcess>;
+}
+
+impl ToMyProcesses for ProcessesIter {
+    fn to_my_processes(self) -> Vec<MyProcess> {
+        self.into_iter()
+            .filter_map(|process| {
+                let Ok(process) = process else {
+                    warn!("Failed to get process: {:?}", process.err());
+                    return None;
+                };
+
+                let Ok(ref stat) = process.stat() else {
+                    warn!(
+                        "Failed to get process stat for PID {}: {:?}",
+                        process.pid(),
+                        process.stat().err()
+                    );
+                    return None;
+                };
+                Some(MyProcess {
+                    name: process.process_name(),
+                    id: process.pid(),
+                    parent_id: stat.ppid,
+                    cpu_percent: stat.cpu_percent().unwrap_or_default(),
+                    memory_bytes: process.memory_bytes(),
+                    state: stat.state().into(),
+                    start_time: stat.starttime().get().ok(),
+                    user: process.username(),
+                    command: process.command(),
+                    files_using: process.using_files(),
+                })
+            })
+            .collect()
+    }
+}
